22ec3bc64dc589df035f17c9e8662d04961b18be
IronGreninja <92992745+IronGreninja@users.noreply.github.com>
2025-04-01 16:08:19 +0530

    Use pyproject.nix

--- flake.nix
+++ flake.nix
@@ -1,20 +1,35 @@
 {
+  description = "An over-engineered todolist";
+
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    pyproject-nix.url = "github:pyproject-nix/pyproject.nix";
+    pyproject-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
-    self,
     nixpkgs,
+    pyproject-nix,
     ...
   }: let
-    forAllSystems = nixpkgs.lib.genAttrs ["x86_64-linux"];
+    project = pyproject-nix.lib.project.loadPyproject {
+      projectRoot = ./.;
+    };
+
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+    python = pkgs.python3;
   in {
-    devShells = forAllSystems (system: {
-      default = nixpkgs.legacyPackages.${system}.callPackage ./nix/shell.nix {};
-    });
-    packages = forAllSystems (system: {
-      default = nixpkgs.legacyPackages.${system}.callPackage ./nix/build.nix {};
-    });
+    devShells.x86_64-linux.default = let
+      arg = project.renderers.withPackages {inherit python;};
+
+      pythonEnv = python.withPackages arg;
+    in
+      pkgs.mkShell {packages = [pythonEnv];};
+
+    packages.x86_64-linux.default = let
+      attrs = project.renderers.buildPythonPackage {inherit python;};
+    in
+      python.pkgs.buildPythonPackage (attrs // {env.CUSTOM_ENVVAR = "hello";});
   };
 }
